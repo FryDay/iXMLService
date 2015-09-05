02837d478da732bbe781e99b1be8c618243da2f1
FryDay <jordanday444@gmail.com>
2015-09-05 00:36:35 -0500

    Added horrible tester program to make sure CL command was working. Will cleanup as needed.

---
+++ Tester/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnClTest = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(93, 64);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(235, 64);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnClTest
+            // 
+            this.btnClTest.Location = new System.Drawing.Point(93, 164);
+            this.btnClTest.Name = "btnClTest";
+            this.btnClTest.Size = new System.Drawing.Size(75, 23);
+            this.btnClTest.TabIndex = 2;
+            this.btnClTest.Text = "button1";
+            this.btnClTest.UseVisualStyleBackColor = true;
+            this.btnClTest.Click += new System.EventHandler(this.btnClTest_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(13, 216);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(649, 262);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(93, 25);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(100, 20);
+            this.txtUrl.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 490);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnClTest);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnClTest;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtUrl;
+    }
+}
+
---
+++ Tester/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tester
+{
+    public partial class Form1 : Form
+    {
+        private iXML.Service iXMLSrv = new iXML.Service();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClTest_Click(object sender, EventArgs e)
+        {
+            bool rtn = false;
+            try
+            {
+                btnClTest.Enabled = false;
+
+                iXMLSrv.SetBaseURL((txtUrl.Text.Trim()));
+                iXMLSrv.SetUserInfo(txtUser.Text.Trim(), txtPassword.Text.Trim());
+                iXMLSrv.SetIpcInfo("/tmp/xmlservicei");
+
+                rtn = iXMLSrv.ExecuteCommand("SNDMSG MSG('TEST XMLSERVICE') TOUSR(" + txtUser.Text.Trim() + ")");
+
+                if (rtn)
+                {
+                    txtOutput.Text = "Command Was Successful. XML Response: " + Environment.NewLine + iXMLSrv.GetLastXmlResponse();
+                }
+                else
+                {
+                    txtOutput.Text = "Command Failed. XML Response: " + Environment.NewLine + iXMLSrv.GetLastXmlResponse();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtOutput.Text = "Error: " + ex.Message;
+            }
+            finally
+            {
+                btnClTest.Enabled = true;
+            }
+
+        }
+    }
+}
